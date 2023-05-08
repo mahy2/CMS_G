fd2362b538de5845ebfb8e02e0c61323fc3f4f90
mahy2 <you@example.com>
2023-05-08 19:37:03 +0300

    hi2

---
+++ lib/data.dart
@@ -0,0 +1,10 @@
+
+// Future<List> getCourses() async {
+//   var meals = await http.get(Uri.parse('http://localhost:3000/students'));
+//   if(meals.statusCode == 200){
+//     var data = json.decode(meals.body);
+//     return data;
+//   } else{
+//     throw Exception("Failed to load data");
+//   }
+// }
